59cb1c7c662dc90ff18c1feb44d870c520a7a592
Israel Sanchez <Israel.pro1994@gmail.com>
2021-05-27 12:33:09 -0500

    created a NavBar Component, added react-router and framer-motion

---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+const NavBar = () => {
+
+    return(
+        <Router>
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    )
+
+}
+
+export default NavBar;
